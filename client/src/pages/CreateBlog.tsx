bd87c560747c0c35cce94c0a875bb83900de7678
arrancy <ruturajchondekar@gmail.com>
2025-05-09 19:29:03 +0530

    added navbar to CreateBlog page

--- client/src/pages/CreateBlog.tsx
+++ client/src/pages/CreateBlog.tsx
@@ -1,23 +1,26 @@
+import { Navbar } from "../components/Navbar";
+
 export function CreateBlog() {
   return (
-    <div className="min-h-screen flex pt-6 flex-col w-screen  bg-purple-100">
+    <div className="min-h-screen flex flex-col w-screen bg-gradient-to-br from-slate-950 to-fuchsia-950">
+      <Navbar></Navbar>
       <div className=" w-3/4 flex flex-col flex-1  mx-auto">
-        <div className="w-full flex flex-row shadow-fuchsia-700 shadow-md rounded-lg px-4 py-4">
+        <div className="w-full flex flex-row shadow-fuchsia-700 bg-white shadow-md rounded-lg px-4 py-4">
           <textarea
-            className="flex-1 resize-none focus:outline-none text-4xl font-bold text-slate-950 "
+            className="flex-1 resize- focus:outline-none text-4xl font-bold text-slate-950 resize-none "
             placeholder="enter your title here"
           ></textarea>
           <button className="bg-fuchsia-900 font-bold text-lg px-3 rounded-3xl my-3  text-fuchsia-100 ">
             done
           </button>
         </div>
-        <div className=" flex flex-row flex-1 my-6 rounded-lg shadow-fuchsia-900 shadow-md ">
+        <div className=" flex flex-row flex-1 my-6 bg-white rounded-lg shadow-fuchsia-900 shadow-md ">
           <textarea
-            className=" flex-1 resize-none px-4 py-2 text-lg font-semibold text-slate-800 focus:outline-none"
+            className=" flex-1 resize-none px-4 py-2 text-lg  font-semibold text-slate-800 focus:outline-none"
             placeholder="enter your blog text here"
           ></textarea>
           <div className="">
-            <button className="font-semibold text-fuchsia-950 text-xl bg-fuchsia-200 p-4 rounded-2xl hover:cursor-pointer hover:bg-fuchsia-300 relative mx-2 top-[88%]">
+            <button className="font-semibold text-fuchsia-950 text-xl bg-fuchsia-200 p-4 rounded-2xl hover:cursor-pointer hover:bg-fuchsia-300 relative mx-2 top-[83%]">
               done
             </button>
           </div>
