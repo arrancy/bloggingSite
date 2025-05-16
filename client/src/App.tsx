50f13484f83c9d87eaf17750bc4a562fabf8a256
arrancy <ruturajchondekar@gmail.com>
2025-05-17 00:04:48 +0530

    started working on the blogs page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,7 @@ import { LandingPage } from "./pages/LandingPage";
 import { Signup } from "./pages/Signup";
 import { Signin } from "./pages/Signin";
 import { CreateBlog } from "./pages/CreateBlog";
+import { Blogs } from "./pages/Blogs";
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
               path="/createBlog"
               element={<CreateBlog></CreateBlog>}
             ></Route>
+            <Route path="/Blogs" element={<Blogs></Blogs>}></Route>
           </Routes>
         </RecoilRoot>
       </BrowserRouter>
--- client/src/pages/Blogs.tsx
+++ client/src/pages/Blogs.tsx
@@ -1,3 +1,21 @@
 export function Blogs() {
-  return <div className="min-h-screen w-full"></div>;
+  return (
+    <>
+      <div className="min-h-screen w-full text-center bg-gradient-to-br from-slate-950 to-purple-950 ">
+        <div className="text-slate-200 text-8xl font-bold relative top-10">
+          Blogs
+        </div>
+        <div className=" w-fit relative top-20 mx-auto">
+          <div className="">
+            <h1 className="text-4xl font-semibold text-purple-50 text-left ">
+              Blog title
+            </h1>
+            <div className="text-purple-300 text-xl font-medium text-left ">
+              this is the big ass blog starting etc
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
