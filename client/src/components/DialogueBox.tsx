f83660a41d2315763fe47f24f730785a8313343d
arrancy <ruturajchondekar@gmail.com>
2025-07-27 02:29:46 +0530

    added send button to the dialogueBox

--- client/src/components/DialogueBox.tsx
+++ client/src/components/DialogueBox.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { motion } from "motion/react";
 import { useExitAnimationState } from "../store/exitAnimation";
+import { ArrowRightIcon, ArrowRightLeft } from "lucide-react";
 
 // enum Tones {
 //   "casual",
@@ -26,7 +27,7 @@ export function DialogueBox() {
     false,
     false,
   ]);
-
+  const [isAnElementChecked, setIsAnElementChencked] = useState<boolean>(false);
   const { exitAnimation } = useExitAnimationState();
 
   return (
@@ -38,11 +39,16 @@ export function DialogueBox() {
         className="bg-purple-900 rounded-lg "
       >
         {tones.map((tone, index) => (
-          <div className="flex space-x-1">
+          <div
+            className={`flex space-x-1  hover:bg-purple-800  ${
+              index === 0 ? `rounded-t-lg` : ``
+            }      `}
+          >
             <input
               type="checkbox"
               checked={checkedElementState[index]}
               onClick={() => {
+                setIsAnElementChencked(true);
                 if (!(checkedElementState[index] === true)) {
                   const newArray = [];
                   for (let i = 0; i <= 3; i++) {
@@ -53,19 +59,27 @@ export function DialogueBox() {
                   setCheckedElementState(newArray);
                 }
               }}
-              className="outline-none border-0 rounded-lg"
+              className="outline-none border-0 rounded-lg m-2 bg-purple-100 checked:bg-purple-900"
             ></input>
             <div
               key={index}
-              className={`text-sm text-slate-400 p-2 hover:bg-purple-800 hover:cursor-pointer   ${
-                index === 0 ? `rounded-t-lg` : index === 3 ? `rounded-b-lg` : ``
-              }      
+              className={`text-sm text-slate-400 p-2  hover:cursor-pointer  
                 `}
             >
               {tone}
             </div>
           </div>
         ))}
+        {isAnElementChecked && (
+          <div className="flex space-x-2 justify-center py-2 hover:bg-purple-800 hover:cursor-pointer group rounded-b-lg transition-all ease-in-out duration-200">
+            <div className="text-sm  text-slate-100 font-semibold group-hover:scale-110 ">
+              send
+            </div>
+            <div className="pt-0.5  group-hover:scale-120">
+              <ArrowRightIcon size={16}></ArrowRightIcon>
+            </div>
+          </div>
+        )}
       </motion.div>
     </>
   );
